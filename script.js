6142e23eb14745b278ba4a700a7c342757fd36da
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-05-02 22:22:57 +0530

    main

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-console.log(ulEl)
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
 
 inputBtn.addEventListener('click',()=>{
    myLeads.push(inputEl.value);
-   inputEl.value ="";
+   inputEl.value ="";   
+   localStorage.setItem("myLeads",JSON.stringify(myLeads))
    renderLeads();
+   console.log(localStorage.getItem("myLeads"))
 });
 function renderLeads(){
 let listItems = ""
