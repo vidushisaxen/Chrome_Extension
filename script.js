df246924ddc2596a2448fe55433df4ae1a4f9384
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-05-03 20:01:23 +0530

    main

--- script.js
+++ script.js
@@ -2,29 +2,55 @@
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn") 
+const tabBtn = document.getElementById("tab-btn")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage)
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads);
+}
+
+
+tabBtn.addEventListener("click",function(){
+    
+    chrome.tabs.query({active: true , currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
+   
+})
+
+
+function render(leads){
+    let listItems = ""
+    for(let i=0;i<leads.length;i++){
+        // console.log(myLeads[i])
+        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `
+        <li>
+        <a target='_blank' href='${leads[i]}'>
+        ${leads[i]}
+        </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+    }
+
+deleteBtn.addEventListener("dblclick",()=>{
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
 inputBtn.addEventListener('click',()=>{
    myLeads.push(inputEl.value);
    inputEl.value ="";   
    localStorage.setItem("myLeads",JSON.stringify(myLeads))
-   renderLeads();
+   render(myLeads);
    console.log(localStorage.getItem("myLeads"))
 });
-function renderLeads(){
-let listItems = ""
-for(let i=0;i<myLeads.length;i++){
-    // console.log(myLeads[i])
-    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-    listItems += `
-    <li>
-    <a target='_blank' href='${myLeads[i]}'>
-    ${myLeads[i]}
-    </a>
-    </li>
-    `
-}
-ulEl.innerHTML = listItems
-}
